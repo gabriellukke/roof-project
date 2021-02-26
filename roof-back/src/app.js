fcc4bc1ec62b33a39f20419c3a810eab3e48e77c
gabrielukke <gabriellukk@outlook.com>
2021-02-26 03:30:21 -0300

    Feat: ExpressJS initial config

---
+++ roof-back/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (_, res) => res.send('Roof Back'));
+
+module.exports = app;
