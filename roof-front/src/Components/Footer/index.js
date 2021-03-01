e2ddbfc7692d716a79bb0b5ce03a1f566ffc008f
Richard <richardsouzarodrigues555@gmail.com>
2021-03-01 16:49:40 -0300

    feat: implemented footer component

---
+++ roof-front/src/Components/Footer/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './styles.css';
+
+import Vector from '../../Assets/icons/Vector.png';
+import instagram from '../../Assets/icons/instagram.png';
+import whatsapp from '../../Assets/icons/whatsapp-1.png';
+
+export default function Footer() {
+	return (
+
+		<footer className='footer-container'>
+			<div>
+				<div>
+					<img src={ Vector } alt="Vector" />
+
+					<div className='contact-container'>
+						<p>
+							<img src={ instagram } alt="Instagram Logo" />
+							Instagram
+						</p>
+						<p>
+							Se você tiver dúvidas, mande uma mensagem para:
+						</p>
+						<p>
+							<img src={ whatsapp } alt="Whatsapp Logo" />
+							99 9999- 9999
+						</p>
+					</div>
+
+					<div className='nav-container'>
+						<p><strong>Navegação</strong></p>
+						<p>Quero aprender</p>
+						<p>Sobre</p>
+						<p>Dúvidas</p>
+						<p>Login</p>
+					</div>
+
+					<div>
+						<p>Termos de uso e privacidade</p>
+					</div>
+				</div>
+
+				<div>
+					<p>Av. Belmiro Amorim, 1593 - Santa Lúcia, Maceió - AL, 57082-000</p>
+				</div>
+			</div>
+		</footer>
+	);
+}
