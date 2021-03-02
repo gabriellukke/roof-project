cc42385759bb710b349e8604005ce52ad160db6e
Richard <richardsouzarodrigues555@gmail.com>
2021-03-02 11:11:15 -0300

    feat: implemented footer and added global.css file

---
+++ roof-front/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+<<<<<<< HEAD
+import './global.css';
+=======
+import './App.css';
+>>>>>>> 048154c4653c84f612eda386e1b5fe6d75769d66
+
+const App = () => (
+  <div>
+    Rôof
+  </div>
+);
+
+export default App;
