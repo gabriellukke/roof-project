40c090efe9606c3b47dc9e66a089fe948e297650
Richard <richardsouzarodrigues555@gmail.com>
2021-03-03 14:11:27 -0300

    feat: implemented depositions component from home page

--- roof-front/src/App.js
+++ roof-front/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './global.css';
+import './App.css';
 
 const App = () => (
   <div>
---
+++ roof-front/src/Components/Depositions/index.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import styles from './index.module.css';
+
+import personImage from '../../Assets/personImage.png';
+
+const Depositions = () => (
+	<section className={styles.container}>
+		
+		<div className={styles.titleContainer}>
+			<h1>
+				Experiências de quem
+				já aprendeu com a gente.
+			</h1>
+		</div>
+		
+		<div className={styles.cardsContainer}>
+			
+			<div className={styles.card}>
+				<div>
+					<header>
+						<img src={ personImage } alt="Person" />
+						<h2>Courtney Henry</h2>
+					</header>
+					<p>
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. 
+						Exercitation veniam consequat sunt nostrud amet.
+					</p>
+				</div>
+			</div>
+			<div className={`${styles.card} ${styles.second}`}>
+				<div>
+					<header>
+						<img src={ personImage } alt="Person" />
+						<h2>Courtney Henry</h2>
+					</header>
+					<p>
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. 
+						Exercitation veniam consequat sunt nostrud amet.
+					</p>
+				</div>
+			</div>
+			<div className={`${styles.card} ${styles.third}`}>
+				<div>
+					<header>
+						<img src={ personImage } alt="Person" />
+						<h2>Courtney Henry</h2>
+					</header>
+					<p>
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. 
+						Exercitation veniam consequat sunt nostrud amet.
+					</p>
+				</div>
+			</div>
+		
+		</div>
+	</section>
+);
+
+export default Depositions;
