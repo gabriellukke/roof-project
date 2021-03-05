e08e54b786b7fe536c91f4c58dfabd670aaf6a52
Thiago Santiago <thiagosantiagoss20@gmail.com>
2021-03-05 14:42:18 -0300

    Feat: Added the initial menu settings

--- roof-front/src/Components/Navbar/index.jsx
+++ roof-front/src/Components/Navbar/index.jsx
@@ -1,30 +1,52 @@
 import React from 'react';
 import Logo from '../../Assets/icons/Vector.png';
+import useMedia from '../../Hooks/useMedia';
 import style from './index.module.css';
 
-const Navbar = () => (
-  <nav className={style.nav}>
-    <div className={style.maxWidth}>
-      <img className={style.logo} src={Logo} alt="logo" />
-      <span className={style.menuBtn} />
+const Navbar = () => {
+  const mobile = useMedia('(max-width: 825px)');
+  const [mobileMenu, setMobileMenu] = React.useState(false);
 
-      <ul className={style.menu}>
-        <li>Quero aprender</li>
-        <li>Consultoria</li>
-        <li>Dúvidas</li>
-        <li>Sobre</li>
-        <li className={style.loginContent}>
-          <i className="fas fa-shopping-cart" />
-          <p>Login</p>
-        </li>
-        <li>
-          <button className={style.deliveryBtn} type="button">
-            Delivery
+  return (
+    <nav className={style.nav}>
+      <div className={style.maxWidth}>
+        <img className={style.logo} src={Logo} alt="logo" />
+        {mobile && (
+          <button
+            type="button"
+            className={style.menuBtn}
+            onClick={() => setMobileMenu(!mobileMenu)}
+          >
+            {mobileMenu ? (
+              <i className={`${style.closeMenuBtn} fas fa-times`} />
+            ) : (
+              'home'
+            )}
           </button>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+        )}
+
+        <ul
+          className={`${mobile ? style.navMobile : style.menu} ${
+            mobileMenu && style.navMobileActive
+          }`}
+        >
+          <li>Quero aprender</li>
+          <li>Consultoria</li>
+          <li>Dúvidas</li>
+          <li>Sobre</li>
+          <li className={style.loginContent}>
+            <i className="fas fa-shopping-cart" />
+            <p>Login</p>
+          </li>
+          <li>
+            <button className={style.deliveryBtn} type="button">
+              Delivery
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
