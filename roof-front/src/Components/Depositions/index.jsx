1a5a8abfc0c313cd9be44f464a34d0d87ce7385d
Thiago Santiago <thiagosantiagoss20@gmail.com>
2021-03-08 16:24:27 -0300

    Fix: Fixed indentation

--- roof-front/src/Components/Depositions/index.jsx
+++ roof-front/src/Components/Depositions/index.jsx
@@ -3,56 +3,53 @@ import personImage from '../../Assets/personImage.png';
 import styles from './index.module.css';
 
 const Depositions = () => (
-	<section className={styles.container}>
-		
-		<div className={styles.titleContainer}>
-			<h1>
-				Experiências de quem
-				já aprendeu com a gente.
-			</h1>
-		</div>
-		
-		<div className={styles.cardsContainer}>
-			
-			<article className={styles.card}>
-				<div>
-					<header>
-						<img src={ personImage } alt="Person" />
-						<h2>Courtney Henry</h2>
-					</header>
-					<p>
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. 
-						Exercitation veniam consequat sunt nostrud amet.
-					</p>
-				</div>
-			</article>
-			<article className={`${styles.card} ${styles.second}`}>
-				<div>
-					<header>
-						<img src={ personImage } alt="Person" />
-						<h2>Courtney Henry</h2>
-					</header>
-					<p>
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. 
-						Exercitation veniam consequat sunt nostrud amet.
-					</p>
-				</div>
-			</article>
-			<article className={`${styles.card} ${styles.third}`}>
-				<div>
-					<header>
-						<img src={ personImage } alt="Person" />
-						<h2>Courtney Henry</h2>
-					</header>
-					<p>
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. 
-						Exercitation veniam consequat sunt nostrud amet.
-					</p>
-				</div>
-			</article>
-		
-		</div>
-	</section>
+  <section className={styles.container}>
+    <div className={styles.titleContainer}>
+      <h1>Experiências de quem já aprendeu com a gente.</h1>
+    </div>
+
+    <div className={styles.cardsContainer}>
+      <article className={styles.card}>
+        <div>
+          <header>
+            <img src={personImage} alt="Person" />
+            <h2>Courtney Henry</h2>
+          </header>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
+        </div>
+      </article>
+      <article className={`${styles.card} ${styles.second}`}>
+        <div>
+          <header>
+            <img src={personImage} alt="Person" />
+            <h2>Courtney Henry</h2>
+          </header>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
+        </div>
+      </article>
+      <article className={`${styles.card} ${styles.third}`}>
+        <div>
+          <header>
+            <img src={personImage} alt="Person" />
+            <h2>Courtney Henry</h2>
+          </header>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
+        </div>
+      </article>
+    </div>
+  </section>
 );
 
 export default Depositions;
