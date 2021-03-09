882fe954a706721c353c3b06d3c7481794a927b3
lucasjesusone <lucasjesusone@gmail.com>
2021-03-08 23:09:22 -0400

    feat/creating course components

--- roof-front/src/App.jsx
+++ roof-front/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Courses from './Components/Courses/Courses';
+
 const App = () => (
   <Router>
     <Switch>
-      <Route />
+      <Route exact="/" component={Courses} />
     </Switch>
   </Router>
 );
