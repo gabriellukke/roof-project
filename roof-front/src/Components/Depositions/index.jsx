b42237e7b2ae9f96616e4764db02e30d6fd04eb6
Richard <richardsouzarodrigues555@gmail.com>
2021-03-12 17:01:18 -0300

    feat: add new styles to depositions component

--- roof-front/src/Components/Depositions/index.jsx
+++ roof-front/src/Components/Depositions/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import personImage from '../../Assets/personImage.png';
+
+import Image1 from './assets/fernanda-alice.png';
+import Image2 from './assets/alisson-de-jesus.png';
+import Image3 from './assets/caroline-pantoja.png';
+
 import styles from './index.module.css';
 
 const Depositions = () => (
@@ -12,39 +16,40 @@ const Depositions = () => (
       <article className={styles.card}>
         <div>
           <header>
-            <img src={personImage} alt="Person" />
-            <h2>Courtney Henry</h2>
+            <img src={Image1} alt="Person" />
+            <h2>Fernanda Alice</h2>
           </header>
           <p>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.
+            A professora Rô foi objetiva e precisa para solucionar meu problema.
+            Eu ficava trocando professores há quase 3 meses até que encontrei
+            ela.
           </p>
         </div>
       </article>
       <article className={`${styles.card} ${styles.second}`}>
         <div>
           <header>
-            <img src={personImage} alt="Person" />
-            <h2>Courtney Henry</h2>
+            <img src={Image2} alt="Person" />
+            <h2>Alisson de Jesus</h2>
           </header>
           <p>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.
+            Quando entrei em contato com ela, não acreditava mais que era
+            possível, pois tive muitas decepções com outros professores. Porém,
+            com a Rô, eu fui levado a sério. Claro que eu tive que fazer minha
+            parte, mas ela soube como me motivar e me ensinar.
           </p>
         </div>
       </article>
       <article className={`${styles.card} ${styles.third}`}>
         <div>
           <header>
-            <img src={personImage} alt="Person" />
-            <h2>Courtney Henry</h2>
+            <img src={Image3} alt="Person" />
+            <h2>Caroline Pantoja</h2>
           </header>
           <p>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-            sint. Velit officia consequat duis enim velit mollit. Exercitation
-            veniam consequat sunt nostrud amet.
+            Só ela sabe como o aluno consegue aprender. A cada aula aprendi
+            coisas que na minha vida inteira de estudante eu achava que não era
+            capaz de aprender. Maravilhosa ! A profe tem uma didática impar.
           </p>
         </div>
       </article>
